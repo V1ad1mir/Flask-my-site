2f1efe17ffba10872bbbec38bfa0d5493432b0d9
VRogi <100993668+VRogi@users.noreply.github.com>
2023-02-07 22:22:11 +0200

    add comments

--- app.py
+++ app.py
@@ -4,20 +4,32 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    '''
+    go to the main page
+    '''
     # if request.method == 'POST':
     #     return request.form['password']
     return render_template('index.html')
 
 @app.route('/map')
-def map():
+def map_page():
+    '''
+    go to the map page
+    '''
     return render_template('map.html')
 
 @app.route('/photos')
 def photos():
+    '''
+    go to photos page
+    '''
     return render_template('photos.html')
 
 @app.route('/reviews')
 def reviews():
+    '''
+    go to reviwes page
+    '''
     return render_template('reviews.html')
 
 
