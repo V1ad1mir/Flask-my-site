d8e8a196d8f6eb9e1dbffc8deb82b41850b7d315
Vrogi <vovak8@gmail.com>
2023-02-08 11:27:01 +0200

    add sql

--- app.py
+++ app.py
@@ -1,15 +1,52 @@
 from flask import Flask,render_template,request
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
+
+#DB configuration
+x = open('db.yaml')
+db = yaml.safe_load(x)
+
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     '''
     go to the main page
     '''
-    # if request.method == 'POST':
-    #     return request.form['password']
+    cursor = mysql.connection.cursor()
+    result_value = cursor.execute("SELECT * FROM users;")
+    if(result_value>0):
+        users = cursor.fetchall()
+        return render_template('index.html',users=users)
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    '''
+    go to the map page
+    '''
+    if request.method == 'POST':
+        form = request.form
+        name = form['username']
+        mail = form['mail']
+        password = form['password']
+        c_password = form['confirm_password']
+        cursor = mysql.connection.cursor()
+        if(password==c_password):
+            try:
+                cursor.execute("INSERT INTO users(name, mail, password) VALUES (%s, %s, %s)", (name,mail,password))
+                mysql.connection.commit()
+                print('OK')
+            except:
+                ...
+    return render_template('register.html')
 
 @app.route('/map')
 def map_page():
