aecf889a4dae460e0606c6ad88928f59eea7919a
Vrogi <vovak8@gmail.com>
2023-03-09 20:04:51 +0200

    change conn function

--- server_operations.py
+++ server_operations.py
@@ -3,18 +3,20 @@ from mysql.connector import Error
 import yaml
 
 def connect():
-    #DB configuration
+    # Load DB configuration from file
     with open('db.yaml') as f:
         db = yaml.safe_load(f)
+    # Connect to database
     try:
-        return mysql.connector.connect(
-            host = db['mysql_host'],
-            user = db['mysql_user'],
-            password = db['mysql_password'],
-            database = db['mysql_db']
+        connection = mysql.connector.connect(
+            host=db['mysql_host'],
+            user=db['mysql_user'],
+            password=db['mysql_password'],
+            database=db['mysql_db']
         )
+        return connection
     except Error as e:
-        print(f"The error '{e}' occurred")
+        raise Exception(f"Could not connect to database. Error: {e}")
 
 def get_all_community_questions():
     with connect() as conn:
